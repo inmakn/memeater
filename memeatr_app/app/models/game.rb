69489ced3cf132ad0c4021aa13ba5eb9c4c35175
inmakn <themousepolice@gmail.com>
2014-08-04 10:54:47 -0400

    removed score validation from game model, added some user auth stuff to application controller, put some seed data in seed file

--- memeatr_app/app/models/game.rb
+++ memeatr_app/app/models/game.rb
@@ -1,9 +1,11 @@
 class Game < ActiveRecord::Base
 
-  validates_presence_of :score, :user_id
-  validates_numericality_of :score, :user_id
+  validates_presence_of :user_id
+  validates_numericality_of :user_id
 
-  belongs_to :user, :character, :environment
+  belongs_to :user
+  belongs_to :character
+  belongs_to :environment
   has_and_belongs_to_many :memes
 
 end

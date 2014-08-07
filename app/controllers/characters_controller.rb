b3d655dd0779fd1b91fd0dcbd2f1284c447a1488
TaliaS1214 <talias432@gmail.com>
2014-08-07 18:22:16 -0400

    Added sound effects and music to game, cleaned up code

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,18 +4,4 @@ class CharactersController < ApplicationController
     @characters = Character.all
   end
 
-  def show
-    @character = Character.find(params[:id])
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def character_params
-    params.require(:character).permit(:spritesheet)
-  end
-
 end
--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -1,19 +1,2 @@
 class EnvironmentsController < ApplicationController
-
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def environment_params
-    params.require(:environment).permit(:top_layer, :middle_layer, :bottom_layer)
-  end
-
 end
